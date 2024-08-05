27bfba105cbe55f9f6fdcc90e3af0cde08e265aa
Mochi8787887 <mochi8787887@gmail.com>
2024-08-05 10:19:24 +0800

    【新增】 先前專案檔

---
+++ Dalle3_OpenURL/Dalle3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Azure;
+using Azure.AI.OpenAI;
+
+
+// 官方文件: https://learn.microsoft.com/zh-tw/azure/ai-services/openai/chatgpt-quickstart?tabs=command-line%2Cpython-new&pivots=programming-language-csharp
+// 參數的網址參考: https://platform.openai.com/docs/guides/images/usage?context=node
+
+namespace Dalle3
+{
+    // 需要安裝 NuGet 套件: Azure.AI.OpenAI (專案使用 1.0.0-beta.17 版本)
+    public partial class GenerateImages
+    {
+        public static async Task Main(string[] args)
+        {
+            // 設定 Azure OpenAI 服務的 endpoint 和 key (明碼的寫法僅供測試使用，實際應用請使用安全的方式存取金鑰)
+            string endpoint = "AOAI 的 端點(endpoint)";
+            string key = "AOAI 的 金鑰(key)";
+
+            OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+
+            for (int i = 0; i < 3; i++)
+            {
+                Response<ImageGenerations> imageGenerations = await client.GetImageGenerationsAsync(
+                    new ImageGenerationOptions()
+                    {
+                        // 下方程式碼需修改 DeploymentName 和 Prompt 內容，其餘參數可依需求調整
+                        DeploymentName = "對應 AOAI 的模型「部署名稱」",
+                        Prompt = "設定圖片的敘述，例如:在街道上盛開的櫻花樹",
+                        Size = ImageSize.Size1792x1024,     //1024x1024, 1024x1792 or 1792x1024
+                        Quality = "hd",                     // standard or hd
+                        Style = "vivid"                     // natural or vivid
+                    });
+
+                // 從 ImageGenerations 回應中取得圖片的 URL
+                Uri imageUri = imageGenerations.Value.Data[0].Url;
+
+                // 將圖片的 URI 輸出:
+                //Console.WriteLine(imageUri);
+
+                // 開啟圖片的 URL
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = imageUri.ToString(),
+                    UseShellExecute = true
+                });
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ KeyVault/KeyVault/Program.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace KeyVault
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 【查看方式一】: Entra識別碼 > 租用戶識別碼
+            // 【查看方式二】: Entra識別碼 > 應用程式註冊 > 應用程式 (用戶端) 識別碼
+            var clientId = "78c8f999-******";
+
+            // Entra識別碼 > 應用程式註冊 > 管理 > 憑證及祕密 > 值
+            var clientSecret = "BYa8Q~******";
+
+            // 【查看方式一】: Entra識別碼 > 概觀 > 租用戶識別碼
+            // 【查看方式二】: Entra識別碼 > 應用程式註冊 > 目錄 (租用戶) 識別碼
+            var tenantId = "d30c40cf-******";
+
+            Environment.SetEnvironmentVariable("AZURE_CLIENT_ID", clientId);
+            Environment.SetEnvironmentVariable("AZURE_CLIENT_SECRET", clientSecret);
+            Environment.SetEnvironmentVariable("AZURE_TENANT_ID", tenantId);
+
+            // Key Vault > 概觀 > 保管庫 URI
+            var kvUri = "https://{Your KeyVault Name}.vault.azure.net/";
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+            // Key Vault > 物件 > 祕密 > 祕密名稱
+            KeyVaultSecret secret = client.GetSecret("{Your Secret Name}");
+
+            Console.WriteLine(secret.Value);
+            Console.ReadKey();
+        }
+    }
+}
